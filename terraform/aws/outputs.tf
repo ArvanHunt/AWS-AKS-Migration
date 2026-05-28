35e0fd01c8a0b2e7fdd6723f0d2e11e2ae4c0e6d
ArvanHunt <arvankumar95@gmail.com>
2026-05-28 19:11:01 +0530

    add AWS EKS terraform configuration

---
+++ terraform/aws/outputs.tf
@@ -0,0 +1,25 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster certificate"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
